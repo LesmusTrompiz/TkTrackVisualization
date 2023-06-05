2f2871b1458639a534fd223f1a4d8b9ed4faeff3
Javier Ortiz Perez-Jaraiz <javierortizpj@gmail.com>
2023-06-05 22:23:46 +0200

    Centrado con imágenes

--- xml_comparer/app.py
+++ xml_comparer/app.py
@@ -1,7 +1,7 @@
 from controller.main_controller import MainController
 from views.main_view import MainView
-from tkinter import Tk, PhotoImage
-from tkinter.ttk import Label, Frame
+from tkinter import Tk, PhotoImage, Label
+from tkinter.ttk import  Frame
 
 
 
@@ -12,22 +12,35 @@ if __name__ == "__main__":
     
     main_controller = MainController(main_view)
     title_frame = Frame(root, relief='ridge', borderwidth=10)
-    label = Label(root, text="COMPARADOR DE XMLS", font={'size' :20})
+    
+    label = Label(title_frame, text="COMPARADOR DE XMLS", font={'size' :30})
+    
+    
+    image_label =  Label(title_frame, width=300,  height=150)
+    image = PhotoImage(file='D:/Proyectos/TkTrackVisualization/xml_comparer/ex2.png')#,width=100, height=200)
+    image_label['image'] = image
+    
+    image_label.grid(row=0, column=3)
     label.grid(row=0, column=0, sticky='NS')#, padx = main_view.winfo_screenwidth()/2)
     
     
-    image_label =  Label(root)
-    image = PhotoImage(file='D:/Proyectos/TkTrackVisualization/xml_comparer/ex.png',width=30, height=30)
-    image_label['image'] = image
     
     
-    image_label.grid(row=0, column=1, sticky='NSEW')
     title_frame.grid(row=0, column=0,  sticky='NSEW')
+    
+    title_frame.columnconfigure(0,weight=1)
+    title_frame.rowconfigure(0,weight=1)
+    
+    
     
     main_view.grid(row=1, column=0, sticky='NSEW')
     root.columnconfigure(0,weight=1)
     root.rowconfigure(1,weight=10)
-    root.rowconfigure(0,weight=1)
+    #root.rowconfigure(0,weight=1)
+    root.update_idletasks() 
+    print(image_label.winfo_width())
+    image_label.winfo_height
+    
     print(main_view.winfo_screenwidth())
     root.mainloop()
     
